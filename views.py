21190e286cd2c0f494ef67ff5c30a774c2c1a275
zebigode0000 <teixeiraqugusto@gmail.com>
2026-03-10 08:57:57 -0300

    asd

--- views.py
+++ views.py
@@ -17,3 +17,27 @@ def estudio():
 @app.route("/atendimento") 
 def atendimento(): 
     return render_template("atendimento.html")
+
+@app.route("/blog") 
+def blog(): 
+    return render_template("blog.html")
+
+@app.route("/cuidados") 
+def cuidados(): 
+    return render_template("blogs/cuidados.html")
+
+@app.route("/estilo") 
+def estilo(): 
+    return render_template("blogs/estilo.html")
+
+@app.route("/primeira") 
+def primeira(): 
+    return render_template("blogs/primeira.html")
+
+@app.route("/vegan") 
+def vegan(): 
+    return render_template("blogs/vegan.html")
+
+@app.route("/realismo") 
+def realismo(): 
+    return render_template("blogs/realismo.html")
